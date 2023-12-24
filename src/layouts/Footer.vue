514a59fb5da82839d68a3f563449e1d070d87481
zzliou <zzliou7579@gmail.com>
2023-12-24 23:40:49 +0800

    新增footer

---
+++ src/layouts/Footer.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="footer">
+    <div class="wrapper">
+      <div class="aboutme">
+        <p>關於</p>
+        <div class="divider"></div>
+        <div class="contactus">
+          <a href="https://assistance.aesop.com/hc/zh-tw/p/contact_us">聯絡我們</a>
+        </div>
+        <div class="location">
+          <a href="https://www.aesop.com/tw/?visitMenu=open">門市據點</a>
+      
+        </div>
+      </div>
+      <div class="social-media">
+        <p>社群媒體</p>
+        <div class="divider"></div>
+        <div class="section">
+          <img :src="facebook" alt="">
+        </div>
+        <div class="section">
+          <img :src="instagram" alt="">
+        </div>
+        <div class="section">
+          <img :src="line" alt="">
+        </div>
+      </div>
+      <div class="designby">
+        <p>©︎ 2023 ALLESGUTE</p>
+        <p>SITE BY ZIZILIOU</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import facebook from "@/assets/img/facebook.png"
+import instagram from "@/assets/img/instagram.png"
+import line from "@/assets/img/line.png"
+</script>
+
+<style lang="scss" scoped>
+.footer {
+  width: 100%;
+  background-color: #252525;
+  .wrapper {
+    display: flex;
+    justify-content: space-evenly;
+  }
+  .aboutme {
+    color: #f5f4e9;
+    width: 300px;
+    height: 300px;
+    font-size: 24px;
+    .divider {
+      width: 100%;
+      border-bottom: #f5f4e9 1px solid;
+      margin: 5px 0px;
+    }
+    a {
+      color: #f5f4e9;
+      font-size: 20px;
+      text-decoration: none;
+    }
+    .contactus {
+      margin: 10px 0px;
+    }
+    .location {
+      margin: 10px 0px;
+    }
+
+  }
+  .social-media {
+    color: #f5f4e9;
+    width: 300px;
+    height: 300px;
+    font-size: 24px;
+    img {
+      width: 30px;
+      height: 30px;
+    }
+    .section {
+      display: inline-block;
+      margin: 10
+      px 5px;
+    }
+    .divider {
+      width: 100%;
+      border-bottom: #f5f4e9 1px solid;
+      margin: 5px 0px;
+    }
+  }
+  .designby {
+    color: #f5f4e9;
+    width: 300px;
+    height: 200px;
+    font-size: 16px;
+    padding-top: 10px;
+  }
+}
+</style>
