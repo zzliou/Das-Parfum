cc3d8cd513bece3c3dedf1f0dd4ed40e7ee37a05
zzliou <zzliou7579@gmail.com>
2024-03-07 17:20:51 +0800

    建置購物車預覽功能

--- src/components/PreviewCart.vue
+++ src/components/PreviewCart.vue
@@ -1,19 +1,40 @@
 <template>
-  <div class="login-dialogue" v-if="isShow">
-    <div>
-    <div 
-      @mouseenter="showCartPreview"
-      @mouseleave="hideCartPreview"
-      @click="goToCartPage" > 購物車 </div>
-    
-    <!-- 购物车预览 -->
-  </div>
+  <div class="previewWrapper" v-if="isShow">
+    <div class="content">
+      <div class="productWrapper">
+        <div class="item" v-for="(product, index) in cartListToShow" :key="index">
+          <div class="name">{{ product.title }}</div>
+          <div class="detailWrapper">
+            <div class="capacity">
+            <div class="title">容量</div>
+            {{ product.sizeList[product.selectedSizeIndex].capacity }} ml
+            </div>
+            <div class="unitPrice">
+            <div class="title">單價</div>
+            {{ product.sizeList[product.selectedSizeIndex].price }}
+            </div>
+            <div class="quantity">
+            <div class="title">數量</div>
+            {{ product.quantity }}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="buttonWrapper" @click="gocheckout">
+        <button>前往結帳</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
-import { defineProps, defineExpose, ref } from 'vue'
-import CartPreview from './PreviewCart.vue'; // 导入购物车预览组件
+import { defineProps, defineExpose, ref, getCurrentInstance } from 'vue';
+import { useRouter } from 'vue-router';
+let globalObject = getCurrentInstance().appContext.config;
+let cartList = globalObject.cartList;
+let cartListToShow = ref(cartList);
+
+const router = useRouter()
 
 defineProps(['title'])
 let isShow = ref(false)
@@ -28,73 +49,77 @@ defineExpose({
   hide: hide
 })
 
-
+function gocheckout() {
+  router.push('/CartshopView')
+}
 
 </script>
 
 <style lang="scss" scoped>
-.login-dialogue {
+.previewWrapper {
   position: fixed;
-  top: 50px;
-  left: 50%;
-  transform: translateX(-50%);
+  top: 110px;
+  right: 0px;
   background-color: $color-4;
-  width: 500px;
-  height: 800px;
+  width: 300px;
+  height: auto;
   z-index: 1;
-  .closed {
-    button {
-      cursor: pointer;
-      margin: 20px 0px 0px 450px;
-      border-style: none;
-      background-color: $color-4;
-      font-size: 22px;
-    }
-  }
-  .wrapper {
-    font-family: sans-serif;
-    padding: 50px;
-    text-align: start;
-    .loginTitle {
-      font-size: 26px;
-      padding: 50px 0px;
-    }
-    .email {
-      font-size: 16px;
-      padding: 10px 0px;
-    }
-    .password {
-      font-size: 16px;
-      padding: 10px 0px;
-    }
-    input {
-      width: 100%;
-      background-color: $color-4;
-      height: 30px;
-      border: none;
-      border-bottom: 1px solid $color-10;
-    }
-    .loginButton {
-      input {
-        margin: 50px 0px;
-        width: 100%;
-        height: 50px;
-        padding: 10px 0px;
-        background-color: $color-11;
-        color: $color-1;
+  border: solid $color-3 2px;
+  font-family: Suisse, sans-serif;
+  .content {
+    .productWrapper {
+      padding: 20px;
+      .item {
+        .name {
+          font-size: 18px;
+        }
+        .detailWrapper {
+          padding: 10px 0px;
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+          font-size: 18px;
+          .capacity {
+            display: flex;
+            align-items: center;
+            
+          }
+          .unitPrice {
+            display: flex;
+            align-items: center;
+          }
+          .quantity {
+            display: flex;
+            align-items: center;
+          }
+          .title {
+            margin-right: 8px;
+          }
+        }
+        &::after {
+        content: "";
+        display: block;
+        border-top: 1px solid $color-3;
+        margin: 10px 0px 20px 0px;
+        }
       }
     }
-    .register {
-      p {
-        color: $color-3;
-      }
+    .buttonWrapper {
+      display: flex;
+      justify-content: center;
       button {
-        margin: 50px 0px;
-        width: 100%;
-        height: 50px;
-        padding: 10px 0px;
+        a {
+          text-decoration: none;
+          color: $color-11;
+        }
+        width: 250px;
+        height: auto;
+        font-size: 18px;
+        font-weight: bold;
         background-color: $color-4;
-        color: $color-11;
+        border: solid 1px $color-3;
+        margin-bottom: 25px;
+        padding: 8px 0px;
       }
     }
   }
